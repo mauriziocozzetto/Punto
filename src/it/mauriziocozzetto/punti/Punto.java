f075682e6419eedfda0781eca70ef5a8de0e6f3f
tetofonta <stefano.fontana.2000@gmail.com>
2018-04-11 09:01:06 +0200

    Refactoring

--- src/it/mauriziocozzetto/punti/Punto.java
+++ src/it/mauriziocozzetto/punti/Punto.java
@@ -7,87 +7,79 @@ import java.io.Serializable;
 import java.util.Scanner;
 
 public class Punto implements Serializable {
-	private float x;
-	private float y;
-	
-	// Costruttore con due argomenti
-	public Punto(float x, float y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	// Getter e setter non mi servono neanche, non li metto
-	
-	// Somma tra due punti (regola del parallelogramma)
-	public Punto somma(Punto p) {
-		return new Punto(this.x + p.x, this.y + p.y);
-	}
-	
-	// Prodotto tra due punti
-	public Punto prodotto(Punto p) {
-		return new Punto(this.x*p.x - this.y*p.y, this.x*p.y + this.y*p.x);
-	}
+    private float x;
+    private float y;
 
-	// Prodotto di uno scalare per un punto, sfrutto il polimorfismo e lascio lo stesso nome
-	public Punto prodotto(float c) {
-		return new Punto(this.x*c, this.y*c);
-	}
+    public Punto(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
 
-	public boolean appartieneAsseY(){
-		return x == 0;
-	}
+    public Punto somma(Punto p) {
+        return new Punto(this.x + p.x, this.y + p.y);
+    }
 
-	public String descrizione(){
-		return "Punto = (" + x + "; " + y + ")";
-	}
+    public Punto prodotto(Punto p) {
+        return new Punto(this.x * p.x - this.y * p.y, this.x * p.y + this.y * p.x);
+    }
 
-	// Metodo non richiesto, mi serve solo come prova
-	public void in() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Inserisci x e y: ");
-		this.x = Float.parseFloat(sc.next());
-		this.y = Float.parseFloat(sc.next());
-		sc.close();
-	}
+    public Punto prodotto(float c) {
+        return new Punto(this.x * c, this.y * c);
+    }
 
-	public void scrivi() {
-		try {
-			ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(new File("Punto.txt")));
-			outputStream.writeObject(this);
-			outputStream.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+    public String descrizione() {
+        return "Punto = (" + x + "; " + y + ")";
+    }
 
-	// Metodo non richiesto
-	public void out() {
-		System.out.println(this);
-	}
-	
-	public Punto simmetricoRispettoAsseX() {
-		return new Punto(this.x, -this.y);
-	}
+    public void out() {
+        System.out.println(this);
+    }
 
-	private static final float[][] quadranti = {{1.0f, 1.0f}, {-1.0f, 1.0f}, {-1.0f, -1.0f}, {1.0f, -1.0f}};
+    public void scrivi() {
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(new File("Punto.txt")));
+            outputStream.writeObject(this);
+            outputStream.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-	private boolean evaluateSector(float[] multiplier){
-		return (this.x*multiplier[0] > 0 && this.y*multiplier[1] > 0);
-	}
+    public void leggi() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inserisci x e y: ");
+        this.x = Float.parseFloat(sc.next());
+        this.y = Float.parseFloat(sc.next());
+        sc.close();
+    }
 
-	public boolean appartienePrimoQuadrante(){
-		return evaluateSector(quadranti[0]);
-	}
+    private static final float[][] quadranti = {{1.0f, 1.0f}, {-1.0f, 1.0f}, {-1.0f, -1.0f}, {1.0f, -1.0f}};
 
-	public boolean appartieneSecondoQuadrante(){
-		return evaluateSector(quadranti[1]);
-	}
+    private boolean evaluateSector(float[] multiplier) {
+        return (this.x * multiplier[0] > 0 && this.y * multiplier[1] > 0);
+    }
 
-	public boolean appartieneTerzoQuadrante(){
-		return evaluateSector(quadranti[2]);
-	}
+    public boolean appartienePrimoQuadrante() {
+        return evaluateSector(quadranti[0]);
+    }
 
-	public boolean appartieneQuartoQuadrante(){
-		return evaluateSector(quadranti[3]);
-	}
+    public boolean appartieneSecondoQuadrante() {
+        return evaluateSector(quadranti[1]);
+    }
+
+    public boolean appartieneTerzoQuadrante() {
+        return evaluateSector(quadranti[2]);
+    }
+
+    public boolean appartieneQuartoQuadrante() {
+        return evaluateSector(quadranti[3]);
+    }
+
+    public boolean appartieneAsseY() {
+        return x == 0;
+    }
+
+    public Punto simmetricoRispettoAsseX() {
+        return new Punto(this.x, -this.y);
+    }
 }
